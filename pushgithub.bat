d2e4ec10e8a0460d5a763c7cea2414c6901562c1
jaquimmanelicigano <jaquimmanelicigano@gmail.com>
2026-04-16 18:11:09 +0200

    Forcar psycopg2

--- pushgithub.bat
+++ pushgithub.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "C:\Users\clash\Desktop\trocas"
 git add .
-git commit -m "Debug"
+git commit -m "Forcar psycopg2"
 git push -f https://github.com/jaquimmanelicigano-lab/Trocas.git master
 pause
